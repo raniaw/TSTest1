01a1793280d7004a20232b45887c84f2a7bf1623
raniaw@web.de <raniaw@web.de>
2022-10-04 18:11:12 +0200

    Add the test 'past simple url'

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,5 +1,11 @@
+import { parse, UrlWithParsedQuery } from "url";
+
 export class Utils {
     public static toUCase(arg:string): string {
         return arg.toUpperCase();
     }
+    public static parseUrl(url: string): UrlWithParsedQuery {
+        return parse(url, true);
+    }
+
 }
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -6,4 +6,12 @@ describe('Utils test suite', () => {
         expect(abc).toBe('ABC');
         console.log('first test work!' , abc );
     });
+    test('past simple url',() => {
+        const parseUrl = Utils.parseUrl('http://localhost:8080/login');
+        expect(parseUrl.href).toBe('http://localhost:8080/login');
+        expect(parseUrl.port).toBe('8080');
+        expect(parseUrl.protocol).toBe('http:');
+        expect(parseUrl.query).toEqual({});
+        console.log('past simple url');
+    });
 });
